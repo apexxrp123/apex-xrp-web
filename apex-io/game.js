9608af494f8caed76c83b8f3c736418866ed9f7c
apexxrp123 <apexxrp123@gmail.com>
2026-09-07 12:23:50 -0700

    Update game.js

--- apex-io/game.js
+++ apex-io/game.js
@@ -2783,6 +2783,10 @@
         }
       } catch (_) {}
     };
+      setInterval(() => {
+      if (!window.apexSock || window.apexSock.readyState !== 1) return;
+      if (state.mode !== "play" || !state.world || !state.world.snakes[0]) return;
+      const p = state.world.snakes[0].pts[0];
       if (!p) return;
       window.apexSock.send(JSON.stringify({ t: "pos", name: state.playerName, x: Math.round(p.x), y: Math.round(p.y) }));
     }, 50);
