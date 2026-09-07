eb007a36967838dd67bd9abe900df422cfc93bbd
apexxrp123 <apexxrp123@gmail.com>
2026-09-07 09:28:01 -0700

    Update game.js

--- apex-io/game.js
+++ apex-io/game.js
@@ -879,6 +879,26 @@
         }
       }
     }
+    if (you && you.alive && you.isPlayer && !w.watch && !w.grace) {
+      const rem = window.apexPeers || {};
+      const hx = you.pts[0].x, hy = you.pts[0].y;
+      const hitR2 = 14 * 14;
+      const names = Object.keys(rem);
+      for (let n = 0; n < names.length && you.alive; n++) {
+        const r = rem[names[n]];
+        if (!r || Date.now() - r.at > 3000) continue;
+        const pts = r.trail || [{ x: r.x, y: r.y }];
+        for (let i = 0; i < pts.length; i++) {
+          const dx = hx - pts[i].x, dy = hy - pts[i].y;
+          if (dx * dx + dy * dy < hitR2) {
+            killSnake(you, w);
+            you.death = "body";
+            toast("Hit " + names[n]);
+            break;
+          }
+        }
+      }
+    }
 
     if (you.alive) {
       if (!w.tape) w.tape = [];
