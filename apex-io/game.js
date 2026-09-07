8b40d3c43eddf72b7549deef8f5045a0536cc15b
apexxrp123 <apexxrp123@gmail.com>
2026-09-07 11:35:27 -0700

    Update game.js

--- apex-io/game.js
+++ apex-io/game.js
@@ -721,8 +721,7 @@
         c: playerDrop && pile > 0 ? "#e7c56a" : "#9fe7c2",
         value: playerDrop && pile > 0 ? +(pile / n).toFixed(4) : 0,
         fromPlayer: !!(playerDrop && pile > 0),
-      });
-    }
+        
     if (s.isPlayer && window.apexSock && window.apexSock.readyState === 1 && pile > 0) {
       window.apexSock.send(JSON.stringify({
         t: "shed",
@@ -730,7 +729,6 @@
         drops: world.dropped.slice(-n).map((d) => ({ x: d.x, y: d.y, value: d.value })),
       }));
     }
-  }
   
   function update(dt) {
     const w = state.world;
