65daaf699fb1d2bd68f7c2ccb924f2d1c2665772
apexxrp123 <apexxrp123@gmail.com>
2026-09-07 19:46:19 -0700

    Update game.js

--- apex-io/game.js
+++ apex-io/game.js
@@ -2324,9 +2324,7 @@
     }
     pushChat("den", state.playerName + " challenged " + target.name + " for " + amt + " XRP.", true);
     toast("Challenge sent to " + target.name);
-    window.apexRoom = "duel-" + [state.playerName, target.name].sort().join("-");
     if (window.apexSock && window.apexSock.readyState === 1) {
-      window.apexSock.send(JSON.stringify({ t: "name", name: state.playerName, room: window.apexRoom }));
       window.apexSock.send(JSON.stringify({ t: "chal", from: state.playerName, to: target.name, amt: amt }));
     }
   });
