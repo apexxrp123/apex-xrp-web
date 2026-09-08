cc44ee5ce5b9460bf32b77f7f85215b8b2043dbb
apexxrp123 <apexxrp123@gmail.com>
2026-09-07 17:18:30 -0700

    Update game.js

--- apex-io/game.js
+++ apex-io/game.js
@@ -841,6 +841,11 @@
       w.food.push(foodSpot(w.map, w.food.length + refill + 280));
       refill++;
     }
+      for (const a of w.snakes) {
+      if (!a.alive) continue;
+      for (const b of w.snakes) {
+        if (a === b || !b.alive) continue;
+        if (w.grace && a.isPlayer) continue;
         const hdx = a.pts[0].x - b.pts[0].x;
         const hdy = a.pts[0].y - b.pts[0].y;
         const headR = (a.radius + (b.radius || 6)) * 0.58;
