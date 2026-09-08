266d841916d7d58ad88ad9361305031aac556771
apexxrp123 <apexxrp123@gmail.com>
2026-09-07 20:25:09 -0700

    Update game.js

--- apex-io/game.js
+++ apex-io/game.js
@@ -1316,13 +1316,25 @@
       }
     });
     if (state.mode === "killcam" && w.kcPeers) {
+      w._kcShown = w._kcShown || {};
       w.kcPeers.forEach((peer) => {
         const trail = peer.pts || [];
         if (trail.length < 2) return;
+        let vis = w._kcShown[peer.name];
+        if (!vis || vis.length !== trail.length) {
+          vis = trail.map((p) => ({ x: p.x, y: p.y }));
+        } else {
+          for (let i = 0; i < trail.length; i++) {
+            vis[i].x += (trail[i].x - vis[i].x) * 0.28;
+            vis[i].y += (trail[i].y - vis[i].y) * 0.28;
+          }
+        }
+        w._kcShown[peer.name] = vis;
         const pa = (peer.skin && peer.skin.a) || "#e7c56a";
         const pb = (peer.skin && peer.skin.b) || pa;
+        const neck = vis[Math.min(4, vis.length - 1)];
         const fake = {
-          pts: trail,
+          pts: vis,
           radius: 7,
           colorA: pa,
           colorB: pb,
@@ -1331,10 +1343,10 @@
           horn: (peer.skin && peer.skin.h) || "none",
           tail: (peer.skin && peer.skin.t) || "none",
           eyes: (peer.skin && peer.skin.e) || "#f5e6a8",
-          dir: Math.atan2(trail[0].y - trail[1].y, trail[0].x - trail[1].x),
+          dir: Math.atan2(vis[0].y - neck.y, vis[0].x - neck.x),
           name: peer.name || "",
         };
-        drawSnake(fake, cam); 
+        drawSnake(fake, cam);
       });
     }
     drawMinimap(w);
